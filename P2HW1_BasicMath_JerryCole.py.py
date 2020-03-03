742b0e1594792fb819b60cf13f0efa5a904d6275
jcole007 <61214328+jcole007@users.noreply.github.com>
2020-03-02 19:57:05 -0500

    Add files via upload

---
+++ P2HW1_BasicMath_JerryCole.py.py
@@ -0,0 +1,17 @@
+#This program demonstrates how to calculate the number of day work
+#The number of day off and the number of hours work.
+#The number of day in a week
+#2/21/2020
+#CTI-110 P2HW1-Basic Math
+#Jerry Cole
+test1 = float(input("Enter a number schedule work day:"))
+test2 = float(input("Enter a number of day off:"))
+#The amonut of hours work per day is 8.
+average = (test1 - test2)
+total = (2*4)
+#The number of sick hours.
+total1 = (2+1)
+#The number of personal hours.
+total2 = (1+1)
+print("The average amonut of hours work, sick and personal" ,average * total + total1 + total2)
+ 
---
+++ P2T1_SalesPrediction_ColeJerry.py.py
@@ -0,0 +1,7 @@
+#Sales Prediction
+#15 Feb 2020
+#CTI-110 P2T1- Sales Prediction
+#Jerry Cole
+originalPrice = int (input ("The item's original price: "))
+profit = originalPrice * .23
+print("The profit is",profit)
